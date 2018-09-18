e967ee48755777562969a14d3772b844a3b5cd77
Alexis Moreau <moreau.alexis@morman.fr>
2018-09-18 10:24:24 +0200

    reservation form + social

--- Site/owl-restaurant/process-reservation-form.php
+++ Site/owl-restaurant/process-reservation-form.php
@@ -1,25 +1,21 @@
 <?php
 // Configure your Subject Prefix and Recipient here
-$subjectPrefix = '[Contact via site web]';
+$subjectPrefix = '[Nouvelle reservation via site web]';
 $emailTo       = '<alexismoreau2@live.fr>';
 $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $name    = stripslashes(trim($_POST['name']));
-    $email   = stripslashes(trim($_POST['email']));
-    $phone = stripslashes(trim($_POST['phone']));
-    $message = stripslashes(trim($_POST['message']));
+    $date    = stripslashes(trim($_POST['date']));
+    $time   = stripslashes(trim($_POST['time']));
+    $person   = stripslashes(trim($_POST['person']));
     if (empty($name)) {
-        $errors['name'] = 'Veuillez rentrer un nom.';
+        $errors['date'] = 'Veuillez rentrer une date.';
     }
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors['email'] = 'Veuillez rentrer un e-mail.';
+        $errors['time'] = 'Veuillez rentrer une heure.';
     }
     if (empty($phone)) {
-        $errors['phone'] = 'Veuillez rentrer un numéro de téléphone.';
-    }
-    if (empty($message)) {
-        $errors['message'] = 'Veuillez rentrer un message.';
+        $errors['person'] = 'Veuillez rentrer le nombre de personnes.';
     }
     // if there are any errors in our errors array, return a success boolean or false
     if (!empty($errors)) {
@@ -28,10 +24,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     } else {
         $subject = "$subjectPrefix $subject";
         $body    = '
-            <strong>Nom: </strong>'.$name.'<br />
-            <strong>E-mail: </strong>'.$email.'<br />
-            <strong>Téléphone: </strong>'.$phone.'<br />
-            <strong>Message: </strong>'.nl2br($message).'<br />
+            <strong>Date: </strong>'.$date.'<br />
+            <strong>Heure: </strong>'.$time.'<br />
+            <strong>Nombre de personnes: </strong>'.$person.'<br />
         ';
         $headers  = "MIME-Version: 1.1" . PHP_EOL;
         $headers .= "Content-type: text/html; charset=utf-8" . PHP_EOL;
@@ -45,7 +40,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
         $headers .= "X-Originating-IP: " . $_SERVER['SERVER_ADDR'] . PHP_EOL;
         mail($emailTo, "=?utf-8?B?" . base64_encode($subject) . "?=", $body, $headers);
         $data['success'] = true;
-        $data['message'] = 'Votre message a bien été envoyé, nous vous répondrons dans les plus brefs délais.';
+        $data['message'] = 'Votre demande de réservation a bien été envoyée, nous vous recontacterons dans les plus brefs délais.';
     }
     // return all our data to an AJAX call
     echo json_encode($data);
